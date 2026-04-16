1cb97b9b0e1af3e564a24cb56f6764861a1f4718
putiz0 <mikayasnerisgmail.com>
2026-04-16 10:41:26 -0300

    limites de camera

--- Assets/scripts/miselania/CamaraBereive.cs
+++ Assets/scripts/miselania/CamaraBereive.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CamaraBereive : MonoBehaviour
 {
     public GameObject Tagter_Obg;
+    Vector3 Tagter_transform;
     public GameObject player_Obg;
+    public List<Transform> limites;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +17,20 @@ public class CamaraBereive : MonoBehaviour
     // Update is called once per frame
      void FixedUpdate()  
     {
-        transform.position = Vector3.Lerp(this.transform.position, new Vector3(Tagter_Obg.transform.position.x, Tagter_Obg.transform.position.y, -10), 2f * Time.fixedDeltaTime);
+        if ((player_Obg.transform.position.x > limites[0].position.x || player_Obg.transform.position.x < limites[1].position.x) && (player_Obg.transform.position.y > limites[2].position.y || player_Obg.transform.position.y < limites[3].position.y))
+        {
+            Tagter_transform = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y, -10);
+        }
+        else if(player_Obg.transform.position.x > limites[0].position.x || player_Obg.transform.position.x < limites[1].position.x)
+        {
+            Tagter_transform = new Vector3 (gameObject.transform.position.x, Tagter_Obg.transform.position.y, -10);
+        }
+        else if(player_Obg.transform.position.y > limites[2].position.y || player_Obg.transform.position.y < limites[3].position.y) {
+            Tagter_transform = new Vector3 (Tagter_Obg.transform.position.x, gameObject.transform.position.y, -10);
+        } 
+        else {
+            Tagter_transform = new Vector3 (Tagter_Obg.transform.position.x, Tagter_Obg.transform.position.y, -10);
+        }
+        transform.position = Vector3.Lerp(this.transform.position, new Vector3(Tagter_transform.x, Tagter_transform.y, -10), 2f * Time.fixedDeltaTime);
     }
 }
